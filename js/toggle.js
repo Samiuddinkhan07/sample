52f496b2add031e012c3f6972cbf6da5162e43c9
samiuddin00 <samiuddinkhan2000@gmail.com>
2022-12-19 18:41:39 +0530

    new changes made in js file

--- js/toggle.js
+++ js/toggle.js
@@ -1,8 +1,21 @@
 function toggleCheck() {
-    if(document.getElementById("toggle").checked === true){
-        document.getElementById("hintImageID").style.display = "none";
-    } else {
-        document.getElementById("hintImageID").style.display = "block";
-    }
+    var toggle = document.getElementById("toggle");
+    var divhide = document.getElementById("hintImageID");
+         
+  if(toggle.checked === true){
+      
+      divhide.style.display = "none";
+      $('#toggleSearchFeildDiv').removeAttr('onclick','searchToggleGuide()')
+        $('.calenderguide').removeAttr('onclick','calenderGuide()')
+        $('.settingGuide').removeAttr('onclick','settingsGuide()')
+        $('.helpguide').removeAttr('onclick','')
+        $('.userMainProfile').removeAttr('onclick','userProfileGuide()')
+  } else {
+      divhide.style.display = "flex";
+       $('#toggleSearchFeildDiv').attr('onclick','searchToggleGuide()')
+        $('.calenderguide').attr('onclick','calenderGuide()')
+        $('.settingGuide').attr('onclick','settingsGuide()')
+        $('.helpguide').attr('onclick','helpGuide()')
+        $('.userMainProfile').attr('onclick','userProfileGuide()')
   }
-  
+}
